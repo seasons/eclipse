4162897dafaa46249949ea3d26cdeee06bddb5c6
Luc Succes <l2succes@gmail.com>
2020-11-30 14:40:47 -0500

    Yaytig

--- src/Text/Text.native.tsx
+++ src/Text/Text.native.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import { View } from "react-native"
+import { Text as RNText } from "react-native"
 
 export const Text = () => {
-  return <View>Hello Native</View>
+  return <RNText>Hello Native</RNText>
 }
