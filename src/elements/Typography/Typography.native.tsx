4e24d3598f9a429670bcf2e799e27ea56aaad620
Luc Succes <l2succes@gmail.com>
2020-12-07 15:59:22 -0500

    fix(native): fixes typography.native

--- src/elements/Typography/Typography.native.tsx
+++ src/elements/Typography/Typography.native.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties } from "react"
 import styled from "styled-components/native"
-import type { StyledComponent } from "styled-components"
+import { color as colorHelper } from "@/helpers/color"
+import type { Color, SansSize, DisplaySize, themeProps } from "@/theme/theme"
 import {
   color,
   ColorProps,
@@ -15,15 +16,9 @@ import {
   space,
   SpaceProps,
   style,
+  textAlign,
   TextAlignProps,
 } from "styled-system"
-
-import type {
-  DisplaySize,
-  SansSize,
-  themeProps,
-  TypeSizes,
-} from "@/theme/theme"
 import { determineFontSizes } from "./determineFontSizes"
 
 /**
@@ -47,12 +42,8 @@ export type FontValue = string | FontDefinition
 export interface FontFamilyProps {
   sans: {
     thin: FontValue
-    regular: FontValue
     medium: FontValue
     bold: FontValue
-  }
-  serif: {
-    medium: FontValue
   }
   display: {
     regular: FontValue
@@ -64,16 +55,12 @@ export interface FontFamilyProps {
  */
 export const fontFamily: FontFamilyProps = {
   sans: {
-    thin: "'ProximaNova-Thin', sans-serif",
-    regular: "'ProximaNova-Medium', sans-serif",
-    medium: "'ProximaNova-Medium', sans-serif",
-    bold: "'ProximaNova-Bold', sans-serif",
-  },
-  serif: {
-    medium: "",
+    thin: "ProximaNovaT-Thin",
+    medium: "ProximaNova-Medium",
+    bold: "ProximaNova-Bold",
   },
   display: {
-    regular: "'Apercu-Mono', sans-serif",
+    regular: "Apercu-Mono",
   },
 }
 
@@ -121,34 +108,20 @@ export interface TextProps
   fontFamily?: string
   style?: CSSProperties
   numberOfLines?: number
-  /**
-   * React Native specific. When `numberOfLines` is set, this prop defines how
-   * text will be truncated. `numberOfLines` must be set in conjunction with
-   * this prop.
-   */
-  ellipsizeMode?: string
-  element?: keyof JSX.IntrinsicElements | React.ComponentType<any>
 }
 
 /** Base Text component for typography */
 export const Text = styled.Text<TextProps>`
   ${({ fontFamily }) => fontFamily && renderFontValue(fontFamily)};
-  text-align: ${(props) => props.textAlign || "left"};
   ${fontSize};
+  ${lineHeight};
   ${color};
   ${display};
   ${maxWidth};
   ${space};
-  ${lineHeight};
+  ${textAlign};
   ${verticalAlign};
-  padding: 0;
-  margin: 0;
 `
-
-/**
- * The supported typefaces
- */
-export type FontTypes = keyof TypeSizes
 
 /**
  * Any valid font family
@@ -156,132 +129,76 @@ export type FontTypes = keyof TypeSizes
 export type FontFamily = typeof themeProps["fontFamily"]
 
 /**
- * Any valid font weight
- */
-export type FontWeights = keyof FontFamily["sans"] | keyof FontFamily["display"]
-
-/**
- * Returns the weight, if given, otherwise it defaults to `regular` unless
- * explicitly given `null` in which case it returns undefined, meaning the
- * weight should be inherited from the component’s parent.
- *
- * @param weight
- */
-function _fontWeight(weight?: null | FontWeights) {
-  if (weight === null) {
-    return undefined
-  }
-  return weight || "regular"
-}
-
-function _selectFontFamilyType(weight?: null | FontWeights, italic?: boolean) {
-  return italic ? "italic" : weight
-}
-
-interface StyledTextProps extends Partial<TextProps> {
-  size: string | string[]
-  weight?: null | FontWeights
-  italic?: boolean
-}
-
-export interface DisplayProps extends Partial<TextProps> {
-  size: DisplaySize
-
-  /**
-   * Explicitly specify `null` to inherit weight from parent, otherwise default
-   * to `regular`.
-   */
-  weight?: null | "regular"
-}
-
-/**
- * Creates a wrapper around the generic `Text` component for a font type defined
- * in the palette’s theme (sans, serif, or display).
- *
- * The component’s props are specified with type parameter `P` and should hold
- * both the component’s specific props (size, weight, italic) as well as all of
- * the generic `Text` component’s props, although as optional.
- *
- * @param fontType
- *        The font type that this text component represents.
- * @param selectFontFamilyType
- *        An optional function that maps weight+italic to a font-family.
- */
-function createStyledText<P extends StyledTextProps>(
-  fontType: keyof FontFamily,
-  selectFontFamilyType: typeof _selectFontFamilyType = _selectFontFamilyType
-): StyledComponent<any, any, any, any> {
-  // @ts-ignore
-  return styled<P>(
-    ({ size, weight, italic, element, ...textProps }: StyledTextProps) => {
-      const fontFamilyType = selectFontFamilyType(_fontWeight(weight), italic)
-      // This is mostly to narrow the type of `fontFamilyType` to remove `null`.
-      if (fontFamilyType === null) {
-        throw new Error("Did not expect `fontType` to be `null`.")
-      }
-      const styles =
-        fontType === "display"
-          ? { letterSpacing: "-1px", ...textProps.style }
-          : textProps.style
-      return (
-        <Text
-          fontFamily={fontFamilyType && fontFamily[fontType][fontFamilyType]}
-          {...determineFontSizes(fontType, size)}
-          // styled-components supports calling the prop `as`, but there are
-          //  issues when passing it into this component named `as`. See
-          //  https://github.com/styled-components/styled-components/issues/2448
-          //  for context.
-          // So we are naming it `element` on the way into this component, and
-          //  renaming it to `as` when we pass it to through.
-          {...(element ? { as: element } : {})}
-          {...textProps}
-          style={styles}
-        />
-      )
-    }
-  )``
-}
-
-/**
  * Sans
  */
 
 export interface SansProps extends Partial<TextProps> {
   italic?: boolean
-
   role?: string
-
   size: SansSize
-
+  color?: Color | string
   /**
    * Explicitly specify `null` to inherit weight from parent, otherwise default
    * to `regular`.
    */
-  weight?: null | "regular" | "medium"
+  weight?: null | "thin" | "medium"
+  onPress?: () => void
 }
 
 /**
- * @example
- *
- * <Sans color="black10" size="3t" weight="medium" italic>Hi</Sans>
+ * The Sans typeface is the main Seasons typeface
  */
-export const Sans = createStyledText<SansProps>("sans", (weight, italic) => {
-  return _selectFontFamilyType(weight, italic)
-})
 
-/**
- * This is our Apercu font used mainly for headers
- *
- * @example
- *
- * <Display color="black10" size="3t">Hi</Display>
- */
-export const Display = createStyledText<DisplayProps>("display")
+export const Sans: React.SFC<SansProps> = (props) => {
+  const { size, weight, numberOfLines } = props
+  const color = props.color
+    ? colorHelper(props.color as any)
+    : colorHelper("black100")
+  return (
+    <Text
+      {...props}
+      fontFamily={fontFamily.sans[weight || "medium"]}
+      {...determineFontSizes("sans", size)}
+      color={color}
+      numberOfLines={numberOfLines}
+    />
+  )
+}
 
-/**
- * @example
- *
- * <Display color="black10" size="3t">Hi</Display>
- */
 Sans.displayName = "Sans"
-Display.displayName = "Display"
+
+export interface DisplayProps extends Partial<TextProps> {
+  role?: string
+  size: DisplaySize
+  color?: Color | string
+  /**
+   * Explicitly specify `null` to inherit weight from parent, otherwise default
+   * to `regular`.
+   */
+  onPress?: () => void
+}
+
+export const Display: React.SFC<DisplayProps> = (props) => {
+  const { size, numberOfLines } = props
+  const color = props.color
+    ? colorHelper(props.color as any)
+    : colorHelper("black100")
+  return (
+    <Text
+      {...props}
+      fontFamily={fontFamily.display.regular}
+      {...determineFontSizes("display", size)}
+      color={color}
+      numberOfLines={numberOfLines}
+    />
+  )
+}
+
+export const LogoText = styled.Text`
+  font-family: "Apercu-Mono";
+  font-size: 20;
+  letter-spacing: 2;
+  line-height: 24;
+  color: white;
+  text-align: center;
+`
