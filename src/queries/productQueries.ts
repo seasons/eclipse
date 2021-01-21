a708e704e437d5f525bb091c0811f42ca46b9e1d
Kieran Gillen <kgillen@gmail.com>
2021-01-21 11:32:35 -0500

    Removes unused query

--- src/queries/productQueries.ts
+++ src/queries/productQueries.ts
@@ -129,35 +129,3 @@ export const GET_PRODUCT = gql`
   }
   ${commonProductVariantFragment}
 `
-
-export const GET_COLLECTION = gql`
-  query GetCollection($collectionID: ID!) {
-    collection(where: { id: $collectionID }) {
-      id
-      slug
-      images
-      title
-      subTitle
-      products {
-        id
-        slug
-        name
-        description
-        retailPrice
-        modelHeight
-        modelSize {
-          id
-          display
-        }
-        brand {
-          id
-          name
-        }
-        images {
-          id
-          url
-        }
-      }
-    }
-  }
-`
