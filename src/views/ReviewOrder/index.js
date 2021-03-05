bc7a108a11e2b5dff5a28cc92a0a1e5c21b782cd
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-03-05 14:13:25 -0500

    remove js files

--- src/views/ReviewOrder/index.js
+++
@@ -1,2 +0,0 @@
-export { ReviewOrderContainer as ReviewOrder } from "./ReviewOrderContainer";
-export { OrderFragment as Order_OrderFragment } from "./queries";
--- src/views/index.js
+++
@@ -1,3 +0,0 @@
-export { Collection } from "./Collection";
-export { ReviewOrder, Order_OrderFragment } from "./ReviewOrder";
-export { OrderConfirmation } from "./OrderConfirmation";
