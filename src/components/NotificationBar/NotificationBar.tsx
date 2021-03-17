40e93b5f2e0d8a01afabf9e1ab6647dbc4f98e02
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-03-17 13:32:14 -0500

    absolutely position Notification Bar

--- src/components/NotificationBar/NotificationBar.tsx
+++ src/components/NotificationBar/NotificationBar.tsx
@@ -21,6 +21,10 @@ const Container = styled.div<{ color: string }>`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
   align-items: center;
   padding: 15px 20px 15px 20px;
 `
