8d7714bfbe9b7deb2b1ec29678f21490a4030072
Faiyam Rahman <faiyam@faiyamrahman.com>
2021-04-05 14:31:35 -0500

    no absolute pos on web notif bar

--- src/components/NotificationBar/NotificationBar.tsx
+++ src/components/NotificationBar/NotificationBar.tsx
@@ -17,14 +17,9 @@ export const NotificationBar: React.FC<NotificationBarProps> = (props) => {
 
 const Container = styled.div<{ color: string }>`
   background-color: ${(p) => p.color};
-  z-index: 100;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
   align-items: center;
   padding: 15px 20px 15px 20px;
 `
