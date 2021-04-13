57923a78c50dba1f1c925aabbafbfaf0daeaeb1b
Daniel Ramirez <javamonn@gmail.com>
2021-04-13 12:01:55 -0400

    fix: ensure product buy title line renders title text with a space

--- src/components/ProductBuyTitleLine/ProductBuyTitleLine.tsx
+++ src/components/ProductBuyTitleLine/ProductBuyTitleLine.tsx
@@ -6,25 +6,24 @@ import { BrandLogoContainer } from "./StyledProductBuyTitleLine"
 
 export const ProductBuyTitleLine = ({ brandName, brandLogoUri }) => (
   <Flex flexDirection="row" alignItems="flex-end" justifyContent="center">
-    <Sans color="black100" size="4" weight="medium">
-      Get it new from
-    </Sans>
     {brandLogoUri ? (
-      <BrandLogoContainer>
-        <ProgressiveImage
-          source={{ uri: brandLogoUri }}
-          style={{ height: 21, width: 116 }}
-          backgroundColor={color("white100")}
-          resizeMode="contain"
-        />
-      </BrandLogoContainer>
+      <>
+        <Sans color="black100" size="4" weight="medium">
+          Get it new from {" "}
+        </Sans>
+        <BrandLogoContainer>
+          <ProgressiveImage
+            source={{ uri: brandLogoUri }}
+            style={{ height: 21, width: 116 }}
+            backgroundColor={color("white100")}
+            resizeMode="contain"
+          />
+        </BrandLogoContainer>
+      </>
     ) : (
       <>
         <Sans color="black100" size="4" weight="medium">
-          {" "}
-        </Sans>
-        <Sans color="black100" size="4" weight="medium">
-          {brandName}
+          Get it new from {brandName}
         </Sans>
       </>
     )}
