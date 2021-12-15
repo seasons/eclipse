2603a2de563baf55c09f28d528d2dbbd4da8577d
Kieran Gillen <kgillen@gmail.com>
2021-12-15 15:20:11 +0100

    Updates to product price

--- src/components/ProductPriceText/index.tsx
+++ src/components/ProductPriceText/index.tsx
@@ -6,14 +6,14 @@ import { SansSize } from "@/theme/theme"
 export const ProductPriceText_Product = gql`
   fragment ProductPriceText_Product on Product {
     rentalPrice
-    retailPrice
+    buyUsedPrice
   }
 `
 
 interface ProductPriceTextProps {
   product: {
     rentalPrice: number
-    retailPrice: number
+    buyUsedPrice: number
   }
   size?: SansSize
 }
@@ -22,10 +22,16 @@ export const ProductPriceText: React.FC<ProductPriceTextProps> = ({
   product,
   size = "3",
 }) => {
-  const { rentalPrice, retailPrice } = product
+  const { rentalPrice, buyUsedPrice } = product
+  let text
+  if (buyUsedPrice && buyUsedPrice > 0) {
+    text = `$${rentalPrice} / mo | $${buyUsedPrice / 100} to buy`
+  } else {
+    text = `$${rentalPrice} / mo`
+  }
   return (
     <Sans size={size} color="black50">
-      ${rentalPrice} / mo | ${retailPrice} retail
+      {text}
     </Sans>
   )
 }
